b490a777bb92268866172775b7a800751a461baa
Olga <hijazin.olga@gmail.com>
2020-03-12 18:44:04 -0800

    Final

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from "react";
 import "./App.css";
-
+import "bootstrap/dist/css/bootstrap.min.css";
 const styles = {
   bold: { fontWeight: "bold" },
   italic: { fontStyle: "italic" },
@@ -12,28 +12,60 @@ const stylings = ["bold", "italic", "underline"];
 const colors = ["yellow", "blue", "red", "black", "purple"];
 
 class App extends Component {
-  render() {
-    const stylingBoxes = stylings.map(style => (
-      <button className="btn btn-light" style={styles[style]} key={style}>
-        {style}
-      </button>
-    ));
+  state = {
+    // bold: false,
+    // italic: false,
+    // underline: false,
+    color: "black"
+  };
+  getColor = color => this.setState({ color: color });
+  getStyling = style => this.setState({ [style]: !this.state[style] });
 
-    const colorBoxes = colors.map(color => (
-      <button
-        style={{ backgroundColor: color, height: 30, width: 30 }}
-        key={color}
-      />
-    ));
+  render() {
+    const colorBoxes = colors.map(color => {
+      const isCurrentColor = color === this.state.color;
+      const size = isCurrentColor ? 40 : 30;
+      return (
+        <button
+          className="btn mx-1 mb-5"
+          style={{ backgroundColor: color, height: size, width: 30 }}
+          key={color}
+          onClick={() => this.getColor(color)}
+        />
+      );
+    });
+    const stylingBoxes = stylings.map(style => {
+      return (
+        <button
+          // className="btn btn-light"
+          className={`btn mx-1 mt-5 ${
+            this.state[style] ? "btn-success" : "btn-outline-success"
+          }`}
+          style={styles[style]}
+          key={style}
+          onClick={() => this.getStyling(style)}
+        >
+          {style}
+        </button>
+      );
+    });
 
     return (
-      <div className="App">
-        <div className="my-3">{stylingBoxes}</div>
-        <textarea />
-        <div className="my-3">{colorBoxes}</div>
-      </div>
+      <body className="mx-5 px-5" style={{ backgroundColor: "pink" }}>
+        <div className="App">
+          <div className="my-3">{stylingBoxes}</div>
+          <textarea
+            style={{
+              color: this.state.color,
+              fontStyle: this.state.italic ? "italic" : "",
+              fontWeight: this.state.bold ? "bold" : "",
+              textDecorationLine: this.state.underline ? "underline" : ""
+            }}
+          />
+          <div className="my-3">{colorBoxes}</div>
+        </div>
+      </body>
     );
   }
 }
-
 export default App;
